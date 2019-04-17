a8c0cad47074f4851b59e479e88c2b18f0090227
Ventouz <potoroo725@gmail.com>
2019-04-17 15:30:31 +0200

    format date

--- dump.sh
+++ dump.sh
@@ -8,7 +8,7 @@
 ################################################################
 
 export PATH=/bin:/usr/bin:/usr/local/bin
-TODAY=`date +"%d%b%Y"`
+TODAY=`date +"%Y-%B-%d"`
 
 ################################################################
 
@@ -22,13 +22,21 @@ mkdir -p ${BACKUP_PATH}/${TODAY}
 for i in $*
 do
 	echo -e "sauvegarde de la base ${i}"
+	
 	mysqldump --verbose ${i} -u backup > ${BACKUP_PATH}/${TODAY}/${i}-${TODAY}.sql
-	#| gzip > ${BACKUP_PATH}/${TODAY}/${i}-${TODAY}.sql.gz
 	if test $? -eq 0
 	then
  		echo "Sauvegarde OK"
 	else
 		echo "Erreur pendant la sauvegarde"
+	fi
+	
+	gzip -rv9 ${BACKUP_PATH}/${TODAY}/${i}-${TODAY}.sql
+	if test $? -eq 0
+	then
+ 		echo "Compression OK"
+	else
+		echo "Erreur pendant la compression"
 	fi
 done
 
