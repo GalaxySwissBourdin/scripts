2e49517accd16d50d5919d329b5d690a6e486f3c
Ventouz <>
2019-04-19 11:28:36 +0200

    ajout commentaires

--- dump.sh
+++ dump.sh
@@ -19,19 +19,28 @@ BACKUP_RETAIN_DAYS=30
 
 mkdir -p ${BACKUP_PATH}/${TODAY}
 
+# Execution pour chaque nom de base passé en argument
 for i in $*
 do
+	# Annonce de la base sauvegardée
 	echo -e "sauvegarde de la base ${i}"
-	
+
+	# Export de la base vers un dossier avec la date
+	# Le nom du fichier créé porte le nom de la base et la date
 	mysqldump --verbose ${i} -u backup > ${BACKUP_PATH}/${TODAY}/${i}-${TODAY}.sql
+
+	# Vérification que l'export s'est déroulé correctement
 	if test $? -eq 0
 	then
  		echo "Sauvegarde OK"
 	else
 		echo "Erreur pendant la sauvegarde"
 	fi
-	
+
+	# Compression des fichiers créés avec le meilleur taux de compression
 	gzip -rv9 ${BACKUP_PATH}/${TODAY}/${i}-${TODAY}.sql
+
+	# Vérification que la compression s'est déroulée correctement
 	if test $? -eq 0
 	then
  		echo "Compression OK"
