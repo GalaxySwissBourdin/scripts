b34feec0c5ac859e3867642edcad8f85f297afbd
Ventouz <>
2019-04-23 10:24:24 +0200

    export hourly

--- dump.sh
+++ dump.sh
@@ -9,6 +9,7 @@
 
 export PATH=/bin:/usr/bin:/usr/local/bin
 TODAY=`date +"%Y-%B-%d"`
+HOURLY=`date +"%Y-%B-%d-%H"`
 
 ################################################################
 
@@ -27,7 +28,7 @@ do
 
 	# Export de la base vers un dossier avec la date
 	# Le nom du fichier créé porte le nom de la base et la date
-	mysqldump --verbose ${i} -u backup > ${BACKUP_PATH}/${TODAY}/${i}-${TODAY}.sql
+	mysqldump --verbose ${i} -u backup > ${BACKUP_PATH}/${TODAY}/${i}-${HOURLY}.sql
 
 	# Vérification que l'export s'est déroulé correctement
 	if test $? -eq 0
@@ -38,7 +39,7 @@ do
 	fi
 
 	# Compression des fichiers créés avec le meilleur taux de compression
-	gzip -rv9 ${BACKUP_PATH}/${TODAY}/${i}-${TODAY}.sql
+	gzip -rv9 ${BACKUP_PATH}/${TODAY}/${i}-${HOURLY}.sql
 
 	# Vérification que la compression s'est déroulée correctement
 	if test $? -eq 0
