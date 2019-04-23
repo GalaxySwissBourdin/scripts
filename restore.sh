faa885798434bfb88bcc12980886ded038621b03
Ventouz <>
2019-04-23 11:49:40 +0200

    ajout test argument et test exécution

--- restore.sh
+++ restore.sh
@@ -4,24 +4,37 @@ export PATH=/bin:/usr/bin:/usr/local/bin
 
 ################################################################
 
-BACKUP_PATH='/root/backups'
+BACKUP_PATH='/root/backups/'
 TODAY=`date +"%Y-%B-%d"`
 
-clear
-
-echo "Entrer le nom de la base a restaurer"
-read $BASE
-sleep 1
-
-echo "Entrer le chemin du fichier .sql.gz"
-read $BASE
-sleep 1
+# echo "Entrer le nom de la base a restaurer"
+# read $BASE
+# sleep 1
+#
+# echo "Entrer le chemin du fichier .sql.gz"
+# read $BASE
+# sleep 1
 
 # Recuperation du nom de fichier de la sauvegarde la plus récente
-#ls -ltrR $TODAY | grep $BASE > $FILE
-LAST_SQL_BACKUP=$(ls $BASE*.sql.gz | tail -n 1)
+# ls -ltrR $TODAY | grep $BASE > $FILE
+# LAST_SQL_BACKUP=$(ls $BASE*.sql.gz | tail -n 1)
 
+if test $# -lt 2
+then
+  echo "Entrez le chemin de la base et le nom de la base"
+  echo "Exemple :"
+  echo "./restore.sh 2019-avril-23/glpi-2019-avril-23-12.sql.gz glpi"
+  exit 99
+fi
 
+FILE="$BACKUP_PATH$1"
 
 # Import du fichier base.sql.gz dans mysql
-gunzip < $LAST_SQL_BACKUP | mysql -u backup $BASE
+gunzip < $1 | mysql -u backup $2 --verbose
+
+if test $? -eq 0
+then
+  echo "Restauration OK"
+else
+  echo "Erreur pendant la restauration"
+fi
