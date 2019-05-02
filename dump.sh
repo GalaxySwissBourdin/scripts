2c6caaa36e41bcec28d37f0b50b18d411238e127
Ventouz <>
2019-05-02 12:00:40 +0200

    index day et index heure

--- dump.sh
+++ dump.sh
@@ -8,8 +8,11 @@
 ################################################################
 
 export PATH=/bin:/usr/bin:/usr/local/bin
-TODAY=`date +"%Y-%B-%d"`
+TODAY=`date +"%j-%Y-%B-%d"`
+INDEX_DAY=`date +"%j"`
 HOURLY=`date +"%Y-%B-%d-%H"`
+INDEX_HOUR=`date +"%H"`
+
 
 ################################################################
 
@@ -28,7 +31,7 @@ do
 
 	# Export de la base vers un dossier avec la date
 	# Le nom du fichier créé porte le nom de la base et la date
-	mysqldump --verbose ${i} -u backup > ${BACKUP_PATH}/${TODAY}/${i}-${HOURLY}.sql
+	mysqldump --verbose ${i} -u backup > ${BACKUP_PATH}/${INDEX_DAY}${TODAY}/${INDEX_HOUR}${i}-${HOURLY}.sql
 
 	# Vérification que l'export s'est déroulé correctement
 	if test $? -eq 0
@@ -39,7 +42,7 @@ do
 	fi
 
 	# Compression des fichiers créés avec le meilleur taux de compression
-	gzip -rv9 ${BACKUP_PATH}/${TODAY}/${i}-${HOURLY}.sql
+	gzip -rv9 ${BACKUP_PATH}/${INDEX_DAY}${TODAY}/${INDEX_HOUR}${i}-${HOURLY}.sql
 
 	# Vérification que la compression s'est déroulée correctement
 	if test $? -eq 0
